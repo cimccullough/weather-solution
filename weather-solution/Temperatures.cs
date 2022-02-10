6dedddc3a87d8e585a01bb926c279d11661cade5
Chris McCullough <chris.ian.mccullough@gmail.com>
2022-02-11 11:32:28 +1300

    initial commit

---
+++ weather-solution/Temperatures.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace weather_solution
+{
+    class Temperatures
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
